1039289bea56954794cac251cd757bc7557aee73
Jake Barrington-Light <30250728@blackpool.ac.uk>
2026-01-21 10:08:10 +0000

    Added time.sleep for delays, set as baseline

--- LandingGearControlSystem/bin/App.py
+++ LandingGearControlSystem/bin/App.py
@@ -1,4 +1,5 @@
 from enum import Enum, auto
+import time
 
 class GearState(Enum):
     UP_LOCKED = auto()
@@ -18,6 +19,7 @@ class LandingGearController:
         if self.state == GearState.UP_LOCKED:
             self.state = GearState.TRANSITIONING_DOWN
             self.log("Gear deploying")
+            time.sleep(1)
             self.state = GearState.DOWN_LOCKED
             self.log("Gear locked down")
         else:
@@ -27,10 +29,13 @@ class LandingGearController:
         if self.state == GearState.DOWN_LOCKED:
             self.state = GearState.TRANSITIONING_UP
             self.log("Gear retracting")
+            time.sleep(1)
             self.state = GearState.UP_LOCKED
             self.log("Gear locked up")
         else:
             self.log("Command rejected")
+
+
 
 controller = LandingGearController()
 controller.command_gear_down()
