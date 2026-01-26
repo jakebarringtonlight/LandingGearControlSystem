ee9364525d691d54d13bad01423478d744cbbd59
Jake Barrington-Light <30250728@blackpool.ac.uk>
2026-01-26 13:29:20 +0000

    Added triple redundancy

--- LandingGearControlSystem/src/App.py
+++ LandingGearControlSystem/src/App.py
@@ -13,8 +13,15 @@ class GearPosition(Enum):
     DOWN = auto()
     UNKNOWN = auto()
 
+class GearFault(Enum):
+    SENSOR_MISMATCH = auto()
+    TIMEOUT_ERROR = auto()
+    HYDRAULIC_FAILURE = auto()
+    NONE = auto()
+
 class GearSensor:
-    def __init__(self):
+    def __init__(self, name):
+        self.name= name
         self.position = GearPosition.UP
     
     def get_position(self):
@@ -23,10 +30,41 @@ class GearSensor:
     def set_position(self, position):
         self.position = position
 
+class TripleRedundancy:
+    def __init__(self, sensors):
+        if len(sensors) == 3:
+            self.sensors = sensors
+        else:
+            return
+    
+    def get_position(self):
+        sensor_position_one = self.sensors[0].get_position()
+        sensor_position_two = self.sensors[1].get_position()
+        sensor_position_three = self.sensors[2].get_position()
+
+        if sensor_position_one == sensor_position_two or sensor_position_one == sensor_position_three:
+            return sensor_position_one
+        if sensor_position_two == sensor_position_three:
+            return sensor_position_two
+        else:
+            return GearPosition.UNKNOWN
+
+    def set_position(self, position):
+        for sensor in self.sensors:
+            sensor.set_position(position)
+
+
 class LandingGearController:
     def __init__(self):
         self.state = GearState.UP_LOCKED
-        self.sensor = GearSensor()
+        self.fault = GearFault.NONE
+
+        sensor_1 = GearSensor("Sensor1")
+        sensor_2 = GearSensor("Sensor2")
+        sensor_3 = GearSensor("Sensor3")
+
+        self.sensor = TripleRedundancy([sensor_1, sensor_2, sensor_3])
+
 
     def log(self, message):
         print(f"[{self.state.name}] {message}")
@@ -34,7 +72,12 @@ class LandingGearController:
     def command_gear_down(self):
 
         position = self.sensor.get_position()
-    
+
+        if position == GearPosition.UNKNOWN:
+            self.fault = GearFault.SENSOR_MISMATCH
+            self.log(f"Fault detected: {self.fault}.")
+            return
+
         if self.state == GearState.DOWN_LOCKED and position == GearPosition.DOWN:
             self.log("Command rejected: gear already down.")
             return
@@ -48,13 +91,17 @@ class LandingGearController:
             self.sensor.set_position(GearPosition.DOWN)
             self.log("Gear locked down.")
         else:
-            self.state = GearState.ERROR 
-            self.sensor.set_position(GearPosition.UNKNOWN)
-            self.log("ERROR")
+            self.log("Invalid command rejected.")
+            
 
     def command_gear_up(self):
 
         position = self.sensor.get_position()
+
+        if position == GearPosition.UNKNOWN:
+            self.fault = GearFault.SENSOR_MISMATCH
+            self.log(f"Fault detected: {self.fault}.")
+            return
 
         if self.state == GearState.UP_LOCKED and position == GearPosition.UP:
             self.log("Command rejected: gear already up.")
@@ -68,10 +115,14 @@ class LandingGearController:
             self.state = GearState.UP_LOCKED
             self.sensor.set_position(GearPosition.UP)
             self.log("Gear locked up.")
-        else:
-            self.state = GearState.ERROR 
-            self.sensor.set_position(GearPosition.UNKNOWN)
-            self.log("ERROR")
+        else: 
+            self.log("Invalid command rejected.")
+
+    def reset_system(self):
+        self.state = GearState.UP_LOCKED
+        self.sensor.set_position(GearPosition.UP)
+        self.fault = GearFault.NONE
+
 
 controller = LandingGearController()
 controller.command_gear_down()
